687ca5229686f3b4dcd6a96a0612b8dd12214c76
karimiumar <look.of.the.day@gmail.com>
2021-11-22 11:02:06 +0530

    Junit Lifecycle callback extension

---
+++ src/main/java/com/umar/apps/junit/timing/TimingExtension.java
@@ -0,0 +1,31 @@
+package com.umar.apps.junit.timing;
+
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.logging.Logger;
+
+public class TimingExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+    private static final Logger logger = Logger.getLogger(TimingExtension.class.getName());
+
+    private static final String START_TIME = "start time";
+
+    @Override
+    public void afterTestExecution(ExtensionContext context) throws Exception {
+        var testMethod = context.getRequiredTestMethod();
+        long startTime = getStore(context).remove(START_TIME, long.class);
+        long duration = System.currentTimeMillis() - startTime;
+        logger.info(() -> String.format("Method [%s] took %s ms.", testMethod.getName(), duration));
+    }
+
+    @Override
+    public void beforeTestExecution(ExtensionContext context) throws Exception {
+        getStore(context).put(START_TIME, System.currentTimeMillis());
+    }
+
+    private ExtensionContext.Store getStore(ExtensionContext context) {
+        return context.getStore(ExtensionContext.Namespace.create(getClass(), context.getRequiredTestMethod()));
+    }
+}
